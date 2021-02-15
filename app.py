e5aa09e22358f2e8a86e7ca41df7cf645bd34fb9
hamsa <hamsa.puttaswamy@gmail.com>
2021-02-15 14:09:20 -0600

    registration form added

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import os
 import shutil
 import csv
 import sys
+import json
+import pymongo
+import urllib
 from flask import Flask,render_template, url_for, flash, redirect, request
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_bootstrap import Bootstrap
-from wtforms import StringField, IntegerField, SubmitField, SelectField
+from wtforms import StringField, IntegerField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -16,52 +19,56 @@ bootstrap = Bootstrap(app)
 # Configurations
 app.config['SECRET_KEY'] = 'blah blah blah blah'
 
-class NameForm(FlaskForm):
-	name = StringField('Name', default="Bruce Springsteen")
+# usr = os.environ['MONGO_DB_USER']
+# pwd = os.environ['MONGO_DB_PASS']
+# client = pymongo.MongoClient("mongodb+srv://" + usr + ":" + pwd + "@firstcluster-obuqd.mongodb.net/test?retryWrites=true&w=majority")
+# db = client['SampleDatabase']
+# collection = db['SampleCollection']
+
+client = pymongo.MongoClient("mongodb+srv://ubs:" + urllib.parse.quote('ubs@12345') + "@cluster0.qxrt7.mongodb.net/ubs?retryWrites=true&w=majority")
+db = client.University_Bazar_db
+
+class registerForm(FlaskForm):
+	firstName = StringField('First Name', validators=[DataRequired()])
+	lastName = StringField('Last Name', validators=[DataRequired()])
+	emailID = StringField('Email ID', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Submit')
 
 # ROUTES!
 @app.route('/',methods=['GET','POST'])
 def index():
-	form = NameForm()
+	form = registerForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		return render_template('index.html',form=form,name=name)
-	return render_template('index.html',form=form,name=None)
+		
+		= db.userData_db.find_one({'Email ID': form.emailID.data})
+		userDataRegistraion = {'FirstName': form.firstName.data, 'LastName': form.lastName.data, 'EmailID': form.emailID.data ,'Password': form.password.data }
+		db.userData_db.insert_one(userDataRegistraion).inserted_id
+		return render_template('register.html',form=form, user=True)
+	return render_template('register.html',form=form,name=None)
 
-@app.route('/help')
-def help():
-	text_list = []
-	# Python Version
-	text_list.append({
-		'label':'Python Version',
-		'value':str(sys.version)})
-	# os.path.abspath(os.path.dirname(__file__))
-	text_list.append({
-		'label':'os.path.abspath(os.path.dirname(__file__))',
-		'value':str(os.path.abspath(os.path.dirname(__file__)))
-		})
-	# OS Current Working Directory
-	text_list.append({
-		'label':'OS CWD',
-		'value':str(os.getcwd())})
-	# OS CWD Contents
-	label = 'OS CWD Contents'
-	value = ''
-	text_list.append({
-		'label':label,
-		'value':value})
-	return render_template('help.html',text_list=text_list,title='help')
+
+
+# db.userData_db.find_one({'Email ID': form.emailID.data})
+
+
+
+
+
+
+
 
 @app.errorhandler(404)
+
 @app.route("/error404")
 def page_not_found(error):
 	return render_template('404.html',title='404')
 
 @app.errorhandler(500)
+
 @app.route("/error500")
 def requests_error(error):
 	return render_template('500.html',title='500')
 
 port = int(os.getenv('PORT', '3000'))
-app.run(host='0.0.0.0', port=port)
+app.run(host='127.0.1.0', port=port)
