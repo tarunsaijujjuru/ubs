4b806e52e5aa85d3f7437900055e7f48e3fdbef9
madhura <madhuramh184@gmail.com>
2021-02-15 13:02:40 -0600

    cleaned the base code by removing unnecessary files and packages

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import csv
 import sys
 from flask import Flask,render_template, url_for, flash, redirect, request
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_bootstrap import Bootstrap
 from wtforms import StringField, IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired
@@ -29,29 +27,7 @@ def index():
 		return render_template('index.html',form=form,name=name)
 	return render_template('index.html',form=form,name=None)
 
-@app.route('/help')
-def help():
-	text_list = []
-	# Python Version
-	text_list.append({
-		'label':'Python Version',
-		'value':str(sys.version)})
-	# os.path.abspath(os.path.dirname(__file__))
-	text_list.append({
-		'label':'os.path.abspath(os.path.dirname(__file__))',
-		'value':str(os.path.abspath(os.path.dirname(__file__)))
-		})
-	# OS Current Working Directory
-	text_list.append({
-		'label':'OS CWD',
-		'value':str(os.getcwd())})
-	# OS CWD Contents
-	label = 'OS CWD Contents'
-	value = ''
-	text_list.append({
-		'label':label,
-		'value':value})
-	return render_template('help.html',text_list=text_list,title='help')
+
 
 @app.errorhandler(404)
 @app.route("/error404")
@@ -63,5 +39,5 @@ def page_not_found(error):
 def requests_error(error):
 	return render_template('500.html',title='500')
 
-port = int(os.getenv('PORT', '3000'))
-app.run(host='0.0.0.0', port=port)
+port = int(os.getenv('PORT', '8000'))
+app.run(host='127.0.1.0', port=port)
