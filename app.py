240059b5052aba06c3d8af67a7549ab1cf534fc2
Jujjuru Tarun Sai <tarunsai@Jujjurus-MacBook-Pro.local>
2021-02-15 17:15:46 -0600

    Add registration page

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ import urllib
 from flask import Flask,render_template, url_for, flash, redirect, request
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
-from wtforms import StringField, IntegerField, SubmitField, SelectField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, SubmitField, SelectField, PasswordField, validators
+import email_validator
+from wtforms import validators, StringField, IntegerField, SubmitField, PasswordField
+from wtforms.validators import InputRequired, Email, DataRequired
+from wtforms.fields.html5 import EmailField
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -20,21 +23,27 @@ client = pymongo.MongoClient("mongodb+srv://ubs:" + urllib.parse.quote('ubs@1234
 db = client.University_Bazar_db
 
 class registerForm(FlaskForm):
-	firstName = StringField('First Name', validators=[DataRequired()])
-	lastName = StringField('Last Name', validators=[DataRequired()])
-	emailID = StringField('Email ID', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	submit = SubmitField('Submit')
+	firstName = StringField('First Name', [validators.DataRequired()])
+	lastName = StringField('Last Name', [validators.DataRequired()])
+	emailID = EmailField('Email ID', [validators.DataRequired(),validators.Email()])
+	password = PasswordField('Password', [validators.DataRequired()])
+	submit = SubmitField('submit')
 
 # ROUTES!
 @app.route('/',methods=['GET','POST'])
 def index():
 	form = registerForm()
 	if form.validate_on_submit():
-		userDataRegistraion = {'FirstName': form.firstName.data, 'LastName': form.lastName.data, 'EmailID': form.emailID.data ,'Password': form.password.data }
-		db.userData_db.insert_one(userDataRegistraion).inserted_id
-		return render_template('register.html',form=form, user=True)
-	return render_template('register.html',form=form,name=None)
+		userData = db.userData_db.find_one({'EmailID': form.emailID.data})
+		if userData is None:
+			userDataRegistraion = {'FirstName': form.firstName.data, 'LastName': form.lastName.data, 'EmailID': form.emailID.data ,'Password': form.password.data }
+			db.userData_db.insert_one(userDataRegistraion).inserted_id
+			msg = "Registration Succesful Please Login"
+			return render_template('register.html',form=form, msg=msg)
+		else:
+			msg ="User already exists please login"
+			return render_template('register.html',form=form, msg=msg)
+	return render_template('register.html',form=form)
 
 @app.errorhandler(404)
 @app.route("/error404")
